5d28723028d1e8c2b86cf90d5dad327baf3c0ab4
dangchuanwen <1151517875@qq.com>
2019-07-01 10:01:37 +0800

    创建项目并完成简单工厂模式

---
+++ 创建型模式/简单工厂/demo.ts
@@ -0,0 +1,48 @@
+
+interface AnimalConstructor {
+    new (className: string, skinColor: string): Animal;
+}
+
+interface Animal{
+    say(): void;
+}
+
+class Dog implements Animal {
+    private className: string;
+    private skinColor: string;
+    constructor(className: string, skinColor: string) {
+        this.className = className;
+        this.skinColor = skinColor;
+    }
+    say(): void {
+        console.log("汪汪～");
+    }
+}
+
+class Cat implements Animal {
+    private className: string;
+    private skinColor: string;
+    constructor(className: string, skinColor: string) {
+        this.className = className;
+        this.skinColor = skinColor;
+    }
+    say(): void {
+        console.log("喵喵～");
+    }
+}
+
+class AnimalSimpleFactory {
+    public static createAnimal(
+        animalConstructor: AnimalConstructor,
+        className: string,
+        skinColor: string
+    ): Animal {
+        return new animalConstructor(className, skinColor);
+    }
+}
+
+let dog = AnimalSimpleFactory.createAnimal(Dog, '汪星人', '黄色的');
+let cat = AnimalSimpleFactory.createAnimal(Cat, '喵星人', '橘色的');
+
+dog.say();
+cat.say();
